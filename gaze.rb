33411fcca4ead0c79cb131ad2baf0abf526d558f
wtetsu <tewatana@gmail.com>
2020-01-13 14:25:16 +0900

    First formula

---
+++ gaze.rb
@@ -0,0 +1,18 @@
+class Gaze < Formula
+  desc "Execute commands for you"
+  homepage "https://github.com/wtetsu/gaze/"
+  url "https://github.com/wtetsu/gaze.git",
+      :tag      => "v0.0.1",
+      :revision => "e28573f53a366c04b114adde140ed735cf472453"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", "dist/gaze", "-v", "cmd/gaze/main.go"
+    bin.install "dist/gaze"
+  end
+
+  test do
+    system "true"
+  end
+end
