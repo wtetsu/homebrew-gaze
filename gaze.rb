4616ad6ff633d9b4d121041ab4fb1158f6c7d0ee
wtetsu <tewatana@gmail.com>
2020-02-18 19:58:26 +0900

    v0.1.1

--- gaze.rb
+++ gaze.rb
@@ -2,8 +2,8 @@ class Gaze < Formula
   desc "Execute commands for you"
   homepage "https://github.com/wtetsu/gaze/"
   url "https://github.com/wtetsu/gaze.git",
-      :tag      => "v0.0.9",
-      :revision => "06b73d2e5d7b9075b43d1797f89d1bf7c7ecc728"
+      :tag      => "v0.1.1",
+      :revision => "d13e12787c77a275cb484bd3e87b9e645cadaeca"
 
   depends_on "go" => :build
 
