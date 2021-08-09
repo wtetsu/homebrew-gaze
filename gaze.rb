0ea3ad1dbd19b8b5129e96689462b8ab75da91e3
wtetsu <tewatana@gmail.com>
2021-08-09 16:44:14 +0900

    Minor improvement

--- gaze.rb
+++ gaze.rb
@@ -1,9 +1,9 @@
 class Gaze < Formula
   desc "Execute commands for you"
-  homepage "https://github.com/wtetsu/gaze/"
-  url "https://github.com/wtetsu/gaze.git",
-      tag:      "v1.0.1",
-      revision: "999ee1998eff563949e95a6e00df38374dee8d65"
+  homepage "https://github.com/wtetsu/gaze"
+  url "https://github.com/wtetsu/gaze/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "04466e28487e43d1f68fd9da749391ff1db8a1aca2761308729680d39600d1cc"
+  license "MIT"
 
   depends_on "go" => :build
 
